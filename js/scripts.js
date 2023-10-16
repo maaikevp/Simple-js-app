ce0064f6c08e30451d229a534feef88b2c41873c
maaikevp <146950822+maaikevp@users.noreply.github.com>
2023-10-16 16:17:23 +0200

    Added manual array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 
-alert('Hello world');
+let pokemonList = [
+    {name: "vulpix", height: 2, type: "fire"}, 
+    {name: "lapras", height: 8,  type: ["water", "ice"]}, 
+    {name: "skitty", height: 2,  type: ["normal"]}
+    ]
 
-let favoriteFood = 'cauliflower';
-document.write(favoriteFood);
+ 
