4669ecca6bb8d5aebf3d9f8bd7cc0ac098c83c59
maaikevp <146950822+maaikevp@users.noreply.github.com>
2023-10-19 12:44:47 +0200

    Display array values
    part of 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
+// alert('Hello world');
 
 let pokemonList = [
     {name: "vulpix", height: 2, type: "fire"}, 
     {name: "lapras", height: 8,  type: ["water", "ice"]}, 
     {name: "skitty", height: 2,  type: ["normal"]}
-    ]
+    ];
 
- 
+let i=0; 
+
+for (;pokemonList[i];){
+    if (pokemonList[i].height >2) { text = pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - wow, that is big!" + "<br>";}
+    else {text = pokemonList[i].name + " (height: " + pokemonList[i].height + ")" +"<br>";}
+    i++;
+    // console.log(text);
+    document.write(text);
+}
