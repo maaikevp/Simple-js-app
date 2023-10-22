03cf8fa8746269a273ff7f44583739a808113c60
maaikevp <146950822+maaikevp@users.noreply.github.com>
2023-10-22 18:29:11 +0200

    IIFE and inside function update (Ex. 1.5)

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
-// alert('Hello world');
+// let pokemonList = [
+//     {name: "vulpix", height: 2, type: "fire"}, 
+//     {name: "lapras", height: 8,  type: ["water", "ice"]}, 
+//     {name: "skitty", height: 2,  type: ["normal"]}
+//     ];
 
-let pokemonList = [
+// let i=0; 
+
+// for (;pokemonList[i];){
+//     if (pokemonList[i].height >2) { text = pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - wow, that is big!" + "<br>";}
+//     else {text = pokemonList[i].name + " (height: " + pokemonList[i].height + ")" +"<br>";}
+//     i++;
+//     // console.log(text);
+//     document.write(text);
+// }
+
+let pokemonRepository = (function () {
+    let pokemonList = [
     {name: "vulpix", height: 2, type: "fire"}, 
     {name: "lapras", height: 8,  type: ["water", "ice"]}, 
-    {name: "skitty", height: 2,  type: ["normal"]}
-    ];
+    {name: "skitty", height: 2,  type: ["normal"]}];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
 
-let i=0; 
+  console.log(pokemonRepository.getAll());
 
-for (;pokemonList[i];){
-    if (pokemonList[i].height >2) { text = pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - wow, that is big!" + "<br>";}
-    else {text = pokemonList[i].name + " (height: " + pokemonList[i].height + ")" +"<br>";}
-    i++;
-    // console.log(text);
-    document.write(text);
-}
+
+  pokemonRepository.add({ name: 'Pikachu', height: 0.4 , type: "electro" });
+  console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } 
