3b98bcaedef8fece5019d6aab6c2c91cd1187a05
maaikevp <146950822+maaikevp@users.noreply.github.com>
2023-10-31 09:47:21 +0100

    Added fetch, polyfill and external datalist
    1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,87 @@
-//IIFE including functions getAll and add
 
-let pokemonRepository = (function() {
-    let repository = [
-      { name: "vulpix", height: 2, type: ["fire"] },
-      { name: "lapras", height: 8, type: ["water", "ice"] },
-      { name: "skitty", height: 2, type: ["normal"]
-      }];
-  
-    function add(pokemon) {
-      if (
-        typeof pokemon === "object" &&
-        "name" in pokemon &&
-        "height" in pokemon &&
-        "types" in pokemon
-      ) {
-        repository.push(pokemon);
-      } else {
-        console.log("pokemon is not correct");
-      }
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
-    function getAll() {
-      return repository;
-    }
-    function addListItem(pokemon) {
-      let pokemonList = document.querySelector(".pokemon-list");
-      let listpokemon = document.createElement("li");
-      let button = document.createElement("button");
-      button.innerText = pokemon.name;
-      button.classList.add("button-class");
-      listpokemon.appendChild(button);
-      pokemonList.appendChild(listpokemon); 
-      button.addEventListener("click", function () {
-        showDetails(pokemon); // Call the showDetails 
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener("click", function(event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
       });
-    }
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
 
-    function showDetails(pokemon) {
-      console.log("Name: " + pokemon.name);
-      // console.log("Height: " + pokemon.height);
-      // console.log("Types: " + pokemon.types.join(", "));
-      // You can add more details as needed
-    }
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
+})();
 
 
-    return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem
-    };
-  })();
-  
-  pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
-  
-  console.log(pokemonRepository.getAll());
-  
-  pokemonRepository.getAll().forEach(function(pokemon) {
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
+});
 
 
 
